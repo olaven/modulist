fe601596b885df3dff637dd96ed1d5871cdeff81
olaven <olavsundfoer@gmail.com>
2019-04-06 19:02:17 +0200

    did some work on location services. Still does not work.

--- app/src/main/java/org/olaven/modulist/service/LocationNotificationService.kt
+++ app/src/main/java/org/olaven/modulist/service/LocationNotificationService.kt
@@ -1,20 +1,37 @@
 package org.olaven.modulist.service
 
 import android.Manifest
-import android.app.*
-import android.content.Context
+import android.app.Service
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.os.IBinder
+import android.os.Looper
 import android.support.v4.app.ActivityCompat
 import android.support.v4.app.NotificationCompat
 import android.support.v4.app.NotificationManagerCompat
+import com.google.android.gms.location.*
 import org.olaven.modulist.App
 import org.olaven.modulist.R
 import kotlin.random.Random
 
 
 class LocationNotificationService : Service() {
+
+    private val locationProviderClient = LocationServices.getFusedLocationProviderClient(applicationContext)
+    private val locationCallback = object: LocationCallback() {
+
+        override fun onLocationResult(locationResult: LocationResult) {
+
+            val location = locationResult.lastLocation
+            sendNotification("location: ${location.altitude} ${location.latitude}")
+        }
+    }
+
+    private val locationRequest = LocationRequest().apply {
+        priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+        interval = 300_000 //5 min
+        fastestInterval = 180_000 // 3 min
+    }
 
     override fun onBind(intent: Intent): IBinder? {
 
@@ -23,11 +40,13 @@ class LocationNotificationService : Service() {
 
     override fun onCreate() {
 
-        if (checkPermission(applicationContext)) {
-            sendNotification("permissions granted for location")
-        } else {
-            sendNotification("permissions NOT granted")
-        }
+        if (checkPermission())
+            locationProviderClient
+                .requestLocationUpdates(locationRequest, locationCallback, Looper.myLooper())
+        else
+            // no permission, wont't run
+            stopSelf()
+
     }
 
     private fun sendNotification(message: String) {
@@ -42,11 +61,10 @@ class LocationNotificationService : Service() {
         notificationManager.notify(Random.nextInt(), notification)
     }
 
-
     //NOTE: notifications are not classified as dangerous. Therefore, it is not requried to check for it.
     // However, location is PROTECTION_NORMAL
-    fun checkPermission(context: Context): Boolean = ActivityCompat.checkSelfPermission(
-            context,
+    private fun checkPermission(): Boolean = ActivityCompat.checkSelfPermission(
+            this,
             Manifest.permission.ACCESS_FINE_LOCATION
         ) == PackageManager.PERMISSION_GRANTED
 }
