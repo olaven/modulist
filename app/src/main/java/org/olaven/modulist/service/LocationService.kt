c6bb48d38fb0811d7d9d70067486b7bbe6f0bfd1
olaven <olavsundfoer@gmail.com>
2019-04-06 12:17:28 +0200

    added menu for editing list

---
+++ app/src/main/java/org/olaven/modulist/service/LocationService.kt
@@ -0,0 +1,12 @@
+package org.olaven.modulist.service
+
+import android.app.Service
+import android.content.Intent
+import android.os.IBinder
+
+class LocationService: Service() {
+
+    override fun onBind(intent: Intent?): IBinder? {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+}
