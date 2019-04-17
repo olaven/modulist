f484d1592cdc8d7541826785e6bf63d3c1a0e2e8
olaven <olavsundfoer@gmail.com>
2019-04-17 16:35:41 +0200

    tutorial videos added

--- app/src/main/java/org/olaven/modulist/TutorialVideo.kt
+++ app/src/main/java/org/olaven/modulist/TutorialVideo.kt
@@ -5,9 +5,14 @@ class TutorialVideo(val title: String, val videoId: String) {
     companion object {
 
         fun getAll() = listOf(
-            TutorialVideo("DEMO", "YQHsXMglC9A"),
-            TutorialVideo("Kurzy", "p_8yK2kmxoo"),
-            TutorialVideo("Hellohello", "EkRRo5DN9lI")
+            TutorialVideo("Adding list", "nTZ56f7I6h4"),
+            TutorialVideo("Managing multiple lists", "VZuX8Bfeabk"),
+            TutorialVideo("Adding items", "Fn-eoBG4j0c"),
+            TutorialVideo("Editing a list", "szYmgL6grYE"),
+            TutorialVideo("Adding location reminders", "RTNrtyvhJ2I"),
+            TutorialVideo("Add calendar-event", "oJleCHBCMd8"),
+            TutorialVideo("Sharing your list", "https://youtu.be/mUtPIyYzHSg"),
+            TutorialVideo("Using Weather Planner", "mUtPIyYzHSg")
         )
     }
 }
